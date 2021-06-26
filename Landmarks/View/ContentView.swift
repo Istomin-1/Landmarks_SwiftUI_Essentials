8506d09944de6b872a51c989932f3b709827ec82
Istomin-1 <74534747+Istomin-1@users.noreply.github.com>
2021-06-26 15:14:56 +0500

    Added processing handling User Input.

---
+++ Landmarks/View/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Mikhail on 25.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+       LandmarkList()
+    }
+}
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
---
+++ Landmarks/View/Landmarks/LandmarkList.swift
@@ -0,0 +1,50 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Mikhail on 25.06.2021.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { lanmark in
+            (!showFavoritesOnly || lanmark.isFavorite)
+        }
+    }
+    var body: some View {
+        NavigationView {
+            List {
+                
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+                
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink(
+                        destination: LandmarkDetail(landmark: landmark)) {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+                
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+//        ForEach(["iPhone SE (2nd generation)", "iPhone 12 mini"], id: \.self) { deviceName in
+            LandmarkList()
+                .environmentObject(ModelData())
+//                .previewDevice(PreviewDevice(rawValue: deviceName))
+//                .previewDisplayName(deviceName)
+        }
+        
+    }
+//}
