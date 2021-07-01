c2c36aac3201bdefe27ab6d830dcf2af6b499fbb
Istomin-1 <74534747+Istomin-1@users.noreply.github.com>
2021-07-01 21:40:16 +0500

    Drawn icons that are flagged when users visit a landmark in their list, and also added an application icon.

---
+++ Landmarks/View/Bages/RotatedBadgeSymbol.swift
@@ -0,0 +1,23 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Mikhail on 01.07.2021.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    var body: some View {
+       BadgeSymbol()
+        .padding(-60)
+        .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
